29dd66bf9b6108809362599e942aa893e578abab
FrenchToblerone54 <FrenchToblerone54@users.noreply.github.com>
2026-04-19 07:25:27 +0000

    feat: include allowInsecure=1 in VMess links; bump v0.10.9

--- updater.py
+++ updater.py
@@ -8,7 +8,7 @@ import logging
 import platform
 import requests
 
-VERSION = "0.10.8"
+VERSION = "0.10.9"
 GITHUB_REPO = "frenchtoblerone54/ghostgate"
 _logger = logging.getLogger("updater")
 _arch = platform.machine()
